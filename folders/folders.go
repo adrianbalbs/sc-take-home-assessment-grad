4f3a319cc887dd85eb9f96651e79aa3c78f57338
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-07-23 19:30:09 +1000

    refactor and fix getAllFolders

--- folders/folders.go
+++ folders/folders.go
@@ -4,24 +4,20 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+// Fetches all of the folders with the matching OrgID
+// There are a few things we can do to refactor the code:
+//   - Remove any instances of unused variable declarations
+//   - There was a bug in the second for loop where the address of &v1
+//     never changes, causing the same Folder item to be appended to the list each time
+//   - We can remove both for loops as it is unecessary to instantiate multiple lists, since
+//     the response from FetchAllFoldersByOrgID has already retrived all of the data
+//   - We can also remove the ffr variable instantiated, and instead directly return the FetchFolderResponse
 func GetAllFolders(req *FetchFolderRequest) (*FetchFolderResponse, error) {
-	var (
-		err error
-		f1  Folder
-		fs  []*Folder
-	)
-	f := []Folder{}
-	r, _ := FetchAllFoldersByOrgID(req.OrgID)
-	for k, v := range r {
-		f = append(f, *v)
+	r, err := FetchAllFoldersByOrgID(req.OrgID)
+	if err != nil {
+		return nil, err
 	}
-	var fp []*Folder
-	for k1, v1 := range f {
-		fp = append(fp, &v1)
-	}
-	var ffr *FetchFolderResponse
-	ffr = &FetchFolderResponse{Folders: fp}
-	return ffr, nil
+	return &FetchFolderResponse{Folders: r}, nil
 }
 
 func FetchAllFoldersByOrgID(orgID uuid.UUID) ([]*Folder, error) {

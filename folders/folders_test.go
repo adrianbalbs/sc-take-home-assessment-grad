2db04473ed838a82ce5034a831f79b66d430fd6c
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-07-24 13:34:11 +1000

    write unit tests for GetAllFolders

--- folders/folders_test.go
+++ folders/folders_test.go
@@ -2,12 +2,43 @@ package folders_test
 
 import (
 	"testing"
-	// "github.com/georgechieng-sc/interns-2022/folders"
-	// "github.com/stretchr/testify/assert"
+
+	"github.com/georgechieng-sc/interns-2022/folders"
+	"github.com/gofrs/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_GetAllFolders(t *testing.T) {
-	t.Run("test", func(t *testing.T) {
-		// your test/s here
+	t.Run("returns all folders when orgID matches", func(t *testing.T) {
+		orgID := uuid.FromStringOrNil(folders.DefaultOrgID)
+		res, err := folders.GetAllFolders(
+			&folders.FetchFolderRequest{
+				OrgID: orgID,
+			})
+		assert.NoError(t, err)
+		assert.NotNil(t, res)
+
+		for _, folder := range res.Folders {
+			assert.Equal(t, orgID, folder.OrgId)
+		}
+	})
+
+	t.Run("returns no folders when orgID does not match", func(t *testing.T) {
+		orgID := uuid.Must(uuid.NewV4())
+		res, err := folders.GetAllFolders(
+			&folders.FetchFolderRequest{
+				OrgID: orgID,
+			})
+		assert.NoError(t, err)
+		assert.Empty(t, res.Folders)
+	})
+
+	t.Run("returns error when orgID is Nil", func(t *testing.T) {
+		res, err := folders.GetAllFolders(
+			&folders.FetchFolderRequest{
+				OrgID: uuid.Nil,
+			})
+		assert.Error(t, err)
+		assert.Nil(t, res)
 	})
 }

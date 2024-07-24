b7b17429a181307fca1b1bba5beb99bc9aca832d
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-07-24 17:47:50 +1000

    complete initial implementation of pagination

--- folders/folders_pagination.go
+++ folders/folders_pagination.go
@@ -1,3 +1,68 @@
 package folders
 
+import (
+	"encoding/base64"
+	"strconv"
+
+	"github.com/gofrs/uuid"
+)
+
 // Copy over the `GetFolders` and `FetchAllFoldersByOrgID` to get started
+
+type FetchFoldersPaginatedRequest struct {
+	Token string
+	Limit int
+	OrgID uuid.UUID
+}
+
+type FetchFoldersPaginatedResponse struct {
+	NextToken string
+	Folders   []*Folder
+}
+
+func GetAllFoldersPaginated(req *FetchFoldersPaginatedRequest) (*FetchFoldersPaginatedResponse, error) {
+	// TODO: Deal with negative limits but assume that a user won't pass a negative limit
+	start := 0
+	if req.Token != "" {
+		index, err := decodeNextIndex(req.Token)
+		if err != nil {
+			return nil, err
+		}
+		start = index
+	}
+
+	folders, err := FetchAllFoldersByOrgID(req.OrgID)
+	if err != nil {
+		return nil, err
+	}
+
+	end := start + req.Limit
+	if end > len(folders) {
+		end = len(folders)
+	}
+
+	nextToken := ""
+	if end != len(folders) {
+		nextToken = encodeNextIndex(end)
+	}
+
+	return &FetchFoldersPaginatedResponse{Folders: folders[start:end], NextToken: nextToken}, nil
+}
+
+func encodeNextIndex(nextIndex int) string {
+	return base64.StdEncoding.EncodeToString([]byte(strconv.Itoa(nextIndex)))
+}
+
+func decodeNextIndex(token string) (int, error) {
+	decodedToken, err := base64.StdEncoding.DecodeString(token)
+	if err != nil {
+		return 0, err
+	}
+	index, err := strconv.Atoi(string(decodedToken))
+	if err != nil {
+		return 0, err
+	}
+
+	return index, nil
+}
+
